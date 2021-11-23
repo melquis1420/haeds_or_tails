963ccd87aa861a442e7fdbae0250372d564b6e5d
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2021-11-23 09:34:06 -0300

    Interface
    Interface creation.

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cara_coroa/jogar.dart';
+import 'package:flutter/material.dart';
+import './jogar.dart';
+
+void main() => runApp(MaterialApp(
+      home: Jogar(),
+      debugShowCheckedModeBanner: false,
+    ));
